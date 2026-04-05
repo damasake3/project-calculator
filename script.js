07d6adf952e0d3238c05b530eb6a198cc2b7460b
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-05 19:07:52 +0800

    Calculator now working
    - Operate(num1,num2,operator) working as intended
    - Result from Operate() also displays correctly
    - Added Number() to num1 and num2 the math functions
    - Number() prevents the numbers to be added as a string
    	- (i.e. 1 + 1 = 11)
    - Other buttons not yet done

--- script.js
+++ script.js
@@ -1,31 +1,79 @@
 const screen = document.querySelector("#screen");
 const buttons = document.querySelector("#buttons");
+screen.innerText = 0;
+
+let num1;
+let operator;
+let num2;
+let opPressed = false;
 
 buttons.addEventListener("click", (e) => {
     let target = e.target;
     console.log(target.id);
     console.log(target.value);
+
+    let classList = target.classList;
+    console.log(target.classList);
+
+    if (classList.contains("num")){
+        console.log("Number pressed");
+        if (opPressed === false) {
+            console.log("Not opPressed.")
+            if (screen.innerText == 0) {
+                screen.innerText = target.value;
+                num1 = Number(screen.innerText);
+            }
+            else {
+                screen.innerText += target.value;
+                num1 = Number(screen.innerText);
+            }
+        }
+        else {
+            console.log("Not opPressed.")
+            if (screen.innerText == 0) {
+                screen.innerText = target.value;
+                num2 = Number(screen.innerText);
+            }
+            else {
+                screen.innerText += target.value;
+                num2 = Number(screen.innerText);
+            }
+        }
+    }
+    else if (classList.contains("operator")){
+        console.log("Operator pressed");
+        console.log(target.value);
+
+        opPressed = true;
+        operator = target.value;
+        screen.innerText = 0;
+        
+    }
+    else if (classList.contains("equal")){
+        console.log("Equal pressed");
+        console.log(target.value);
+        console.log(`${num1} ${operator} ${num2}`);
+
+        screen.innerText = operate(num1, num2, operator);
+        num1 = screen.innerText;
+    }
+    else {
+        console.log(`${target.id} pressed`);
+    }
+
 });
 
-let num1;
-let operator;
-let num2;
-
-num1 = 3;
-num2 = 5;
-operator = "+"
-
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 //When User presses `=` button
@@ -41,5 +89,3 @@ function operate(num1, num2, operator) {
             return divide(num1, num2);
     }
 }
-
-console.log(operate(num1, num2, operator));
