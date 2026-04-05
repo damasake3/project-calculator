f2e71a6fb31676b833508fd0fd84625b8dbf1e55
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-05 14:24:08 +0800

    Created the basic functions of a calculator
    - Can take two numbers and an operator
    - Operate() operations depends on the operator
    - e.g. `if it's "+" then add(num1,num2)`

---
+++ script.js
@@ -0,0 +1,35 @@
+let num1;
+let operator;
+let num2;
+
+num1 = 3;
+num2 = 5;
+operator = "+"
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate(num1, num2, operator));
