d9e09f41712545d51398955c121383bfab5de7a6
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-05 20:11:13 +0800

    Refactored Calculator Code for Readability
    - Converted the large nested if else to separate functions
    - Used Switch Cases with function calls

--- script.js
+++ script.js
@@ -13,55 +13,83 @@ buttons.addEventListener("click", (e) => {
     console.log(target.value);
 
     let classList = target.classList;
-    console.log(target.classList);
 
-    if (classList.contains("num")){
-        console.log("Number pressed");
-        if (opPressed === false) {
-            console.log("Not opPressed.")
-            if (screen.innerText == 0) {
-                screen.innerText = target.value;
-                num1 = Number(screen.innerText);
-            }
-            else {
-                screen.innerText += target.value;
-                num1 = Number(screen.innerText);
-            }
-        }
-        else {
-            console.log("Not opPressed.")
-            if (screen.innerText == 0) {
-                screen.innerText = target.value;
-                num2 = Number(screen.innerText);
-            }
-            else {
-                screen.innerText += target.value;
-                num2 = Number(screen.innerText);
-            }
-        }
-    }
-    else if (classList.contains("operator")){
-        console.log("Operator pressed");
-        console.log(target.value);
-
-        opPressed = true;
-        operator = target.value;
-        screen.innerText = 0;
-        
-    }
-    else if (classList.contains("equal")){
-        console.log("Equal pressed");
-        console.log(target.value);
-        console.log(`${num1} ${operator} ${num2}`);
-
-        screen.innerText = operate(num1, num2, operator);
-        num1 = screen.innerText;
-    }
-    else {
-        console.log(`${target.id} pressed`);
+    switch (classListScan(classList)) {
+        case "num":
+            numOps(target, opPressed);
+            break;
+        case "operator":
+            operatorOps(target);
+            break;
+        case "equal":
+            equalOps(target);
+            break;
+        case "special":
+            console.log("special");
+            break;
     }
 
 });
+
+function classListScan(classList) {
+    if (classList.contains("num")) {
+        return "num";
+    }
+    else if (classList.contains("operator")) {
+        return "operator";
+    }
+    else if (classList.contains("equal")) {
+        return "equal";
+    }
+    else {
+        return "special";
+    }
+}
+
+function numOps(target, opPressed) {
+    console.log("Number Pressed");
+    if (opPressed === false) {
+        console.log("Not opPressed.")
+        if (screen.innerText == 0) {
+            screen.innerText = target.value;
+            num1 = Number(screen.innerText);
+        }
+        else {
+            screen.innerText += target.value;
+            num1 = Number(screen.innerText);
+        }
+    }
+    else {
+        console.log("Not opPressed.")
+        if (screen.innerText == 0) {
+            screen.innerText = target.value;
+            num2 = Number(screen.innerText);
+        }
+        else {
+            screen.innerText += target.value;
+            num2 = Number(screen.innerText);
+        }
+    }
+
+}
+
+function operatorOps(target) {
+    console.log("Operator pressed");
+    console.log(target.value);
+
+    opPressed = true;
+    operator = target.value;
+    screen.innerText = 0;
+}
+
+function equalOps(target) {
+    console.log("Equal pressed");
+    console.log(target.value);
+    console.log(`${num1} ${operator} ${num2}`);
+
+    screen.innerText = operate(num1, num2, operator);
+    num1 = screen.innerText;
+}
 
 function add(num1, num2) {
     return Number(num1) + Number(num2);
